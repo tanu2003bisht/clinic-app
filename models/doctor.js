464ad8e335429c230767d3fb7e5abe83a20c7141
Tanu Bisht <tbisht939@gmail.com>
2026-05-07 20:34:33 +0530

    add medicine model

---
+++ models/doctor.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    department:{
+        type: String,
+        enum: ['surgeon','cardio','gyne'],
+        required: true
+    }
+});
+
+const Doctor = mongoose.model('Doctor', doctorSchema);
+module.exports = Doctor;
---
+++ models/medicine.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const medicineSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    category:{
+        type: String,
+        enum: ['antibiotic','painkiller','supplement'],
+        required: true
+    },
+    is_tablet:{
+        type: Boolean,
+        required: true
+    }
+});
+
+const Medicine = mongoose.model('Medicine', medicineSchema);
+module.exports = Medicine;
